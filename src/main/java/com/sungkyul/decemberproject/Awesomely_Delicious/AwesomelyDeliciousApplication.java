09d8239cae5e1eb58bfdcbc22bc035e088a06c82
jiminsong490 <misosjm@naver.com>
2022-12-01 10:31:09 +0900

    project start

---
+++ src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/AwesomelyDeliciousApplication.java
@@ -0,0 +1,13 @@
+package com.sungkyul.decemberproject.Awesomely_Delicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomelyDeliciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomelyDeliciousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungkyul/decemberproject/Awesomely_Delicious/AwesomelyDeliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungkyul.decemberproject.Awesomely_Delicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomelyDeliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

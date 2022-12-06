00820e9040442b9d362d7bd09bb1b037c1149e27
jiminsong490 <misosjm@naver.com>
2022-12-06 16:15:08 +0900

    UserRepository 인터페이스 생성

--- src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/controller/MemberController.java
+++ src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/controller/MemberController.java
@@ -1,13 +1,9 @@
-package com.sungkyul.decemberproject.Awesomely_Delicious.domain.controller;
+package com.sungkyul.decemberproject.Awesomely_Delicious.controller;
 
-import com.sungkyul.decemberproject.Awesomely_Delicious.domain.User;
-import com.sungkyul.decemberproject.Awesomely_Delicious.domain.repository.MemberRepository;
+import com.sungkyul.decemberproject.Awesomely_Delicious.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController

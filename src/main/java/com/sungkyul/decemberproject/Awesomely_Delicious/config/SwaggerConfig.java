78612feff3ee03ffaf921d8d572262a39797f5fe
jiminsong490 <misosjm@naver.com>
2022-12-11 03:06:01 +0900

    Swagger 관련 코드는 살리고 실행은 안시킨 버전

--- src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/config/SwaggerConfig.java
+++ src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/config/SwaggerConfig.java
@@ -1,33 +1,34 @@
-package com.sungkyul.decemberproject.Awesomely_Delicious.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-@Configuration
-@EnableWebMvc
-public class SwaggerConfig {
-    @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sungkyul.decemberproject.Awesomely_Delicious"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Awesomely_Delicious")
-                .description("API 명세서 입니다.")
-                .version("0.0.1")
-                .build();
-    }
-}
+//package com.sungkyul.decemberproject.Awesomely_Delicious.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration
+//@EnableWebMvc
+////http://localhost:8080/swagger-ui/index.html
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.sungkyul.decemberproject.Awesomely_Delicious"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Awesomely_Delicious")
+//                .description("API 명세서 입니다.")
+//                .version("0.0.1")
+//                .build();
+//    }
+//}

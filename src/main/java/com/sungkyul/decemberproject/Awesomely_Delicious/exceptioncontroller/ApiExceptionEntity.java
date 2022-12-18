caf6818a3879feb355d84ae58d75eb77267fec51
dbrjs4594283 <dbrjs4594283@naver.com>
2022-12-18 15:39:49 +0900

    Refactor#28
    회원가입시 이메일 중복 예외처리

---
+++ src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/exceptioncontroller/ApiExceptionEntity.java
@@ -0,0 +1,22 @@
+package com.sungkyul.decemberproject.Awesomely_Delicious.exceptioncontroller;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 오류 메시지 형식 지정
+ */
+@Getter
+@ToString
+public class ApiExceptionEntity {
+    private String errorCode;
+    private String errorMessage;
+
+    @Builder
+    public ApiExceptionEntity(HttpStatus status, String errorCode, String errorMessage){
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+}
--- src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/service/UserService.java
+++ src/main/java/com/sungkyul/decemberproject/Awesomely_Delicious/service/UserService.java
@@ -1,19 +1,29 @@
 package com.sungkyul.decemberproject.Awesomely_Delicious.service;
 
+import com.sungkyul.decemberproject.Awesomely_Delicious.exceptioncontroller.ExceptionEnum;
+import com.sungkyul.decemberproject.Awesomely_Delicious.exceptioncontroller.ApiException;
 import com.sungkyul.decemberproject.Awesomely_Delicious.api.UserForm;
 import com.sungkyul.decemberproject.Awesomely_Delicious.domain.User;
 import com.sungkyul.decemberproject.Awesomely_Delicious.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final MemberRepository memberRepository;
+    private final EntityManager em;
 
     public Long join(UserForm form){
+
         User user = new User();
+        List<User> result = em.createQuery("select m from User m where m.email = : email", User.class)
+                .setParameter("email", form.getEmail())
+                .getResultList();
+        if (!result.isEmpty()) throw new ApiException(ExceptionEnum.Duplicated_Email);
         user.setEmail(form.getEmail());
         user.setPassword(form.getPassword());
         user.setNickname(form.getNickname());
